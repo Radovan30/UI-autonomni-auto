a407bfc995c541962408f896df2cb9a2d8fed085
Radovan <55534148+Radovan30@users.noreply.github.com>
2022-11-06 23:25:32 +0100

    dokoncene kolize

--- car.js
+++ car.js
@@ -20,8 +20,46 @@ class Car {
     }
 
     update( roadBorders ) {
-        this.#move();
+        if ( !this.damaged ) {
+            this.#move();
+            this.polygon = this.#createPolygon();
+            this.damaged = this.#assessDamage( roadBorders );
+        }
         this.sensor.update( roadBorders );
+    }
+    
+    #assessDamage( roadBorders ) {
+        for ( let i = 0; i < roadBorders.length; i++ ) {
+            if ( polysIntersect( this.polygon, roadBorders[ i ] ) ) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    // vytvoreni polygonu na detekovani kolizi
+    #createPolygon() {
+        const points = [];
+        const rad = Math.hypot( this.width, this.height ) / 2;
+        const alpha = Math.atan2( this.width, this.height );
+        points.push( {
+            x: this.x - Math.sin( this.angle - alpha ) * rad,
+            y: this.y - Math.cos( this.angle - alpha ) * rad
+        } );
+        points.push( {
+            x: this.x - Math.sin( this.angle + alpha ) * rad,
+            y: this.y - Math.cos( this.angle + alpha ) * rad
+        } );
+        points.push( {
+            x: this.x - Math.sin( Math.PI + this.angle - alpha ) * rad,
+            y: this.y - Math.cos( Math.PI + this.angle - alpha ) * rad
+        } );
+        points.push( {
+            x: this.x - Math.sin( Math.PI + this.angle + alpha ) * rad,
+            y: this.y - Math.cos( Math.PI + this.angle + alpha ) * rad
+        } );
+        return points;
+        
     }
 
     // posun auta, rychlost a akcelerace
@@ -68,23 +106,19 @@ class Car {
 
     // vykresleni auta do canvas
     draw( context ) {
-        context.save();
-        context.translate( this.x, this.y );
-        context.rotate( -this.angle );
-
+        if ( this.damaged ) {
+            context.fillStyle = "gray";
+        } else {
+            context.fillStyle = "black";
+        }
         context.beginPath();
-        context.rect(
-            -this.width / 2,
-            -this.height / 2,
-            this.width,
-            this.height
-        );
+        context.moveTo( this.polygon[ 0 ].x, this.polygon[ 0 ].y );
+        for ( let i = 1; i < this.polygon.length; i++ ) {
+            context.lineTo( this.polygon[ i ].x, this.polygon[ i ].y );
+        }
         context.fill();
 
-        context.restore();
-
         this.sensor.draw( context );
-
 
         /*
         context.save();
